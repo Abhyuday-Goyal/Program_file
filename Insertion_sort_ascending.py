08192930937a4381daf1074e5f6033afefa1ebe6
Abhyuday-Goyal <107510592+Abhyuday-Goyal@users.noreply.github.com>
2022-08-07 00:22:16 +0530

    Update Insertion_sort_ascending.py

--- Insertion_sort_ascending.py
+++ Insertion_sort_ascending.py
@@ -1,1 +1,12 @@
-
+L= input("enter a list")
+print("unsorted lsi is",L)
+l=len(L)
+for i in range(1,l):
+    key=L[i]
+    j=i-1
+    while j>=0 and key<L[j]:
+        L[j+1]=L[j]
+        j=j-1
+    else:
+        L[j+1]=key
+print("sorted list is", L)
