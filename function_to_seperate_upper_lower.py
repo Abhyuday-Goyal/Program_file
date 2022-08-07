3f2801b39575c92cf2cfcec9ae83b0324c0330b1
Abhyuday-Goyal <107510592+Abhyuday-Goyal@users.noreply.github.com>
2022-08-08 00:08:28 +0530

    Create function_to_seperate_upper_lower.py

---
+++ function_to_seperate_upper_lower.py
@@ -0,0 +1,13 @@
+def sentence(x):
+  l = len(x)
+  str1 =''
+  str2 =''
+  for i in range(0,l):
+    if x[i].upper() == True:
+      str1 = str1+x[i]
+    elif x[i].lower == False:
+      str2 = str2+x[i]
+  return str1,str2
+  
+a = str(input("enter a string "))
+sentence(a)
