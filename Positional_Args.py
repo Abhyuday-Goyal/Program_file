b526f6021771051ca91992915feb9d82aa75d90c
Abhyuday-Goyal <107510592+Abhyuday-Goyal@users.noreply.github.com>
2022-08-09 00:12:20 +0530

    Create Positional_Args.py

---
+++ Positional_Args.py
@@ -0,0 +1,8 @@
+def position_arg(a,b,c,d):
+  sum = a+b+c+d
+  return sum 
+w = int(input("enter the first number"))
+x = int(input("enter the second number"))
+y = int(input("enter the third number"))
+z = int(input("enter the fourth number"))
+position_arg(w,x,y,z)
