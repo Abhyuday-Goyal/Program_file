555d676f78e7d45112cb0f60a41c28274a647f7f
Abhyuday-Goyal <107510592+Abhyuday-Goyal@users.noreply.github.com>
2022-08-10 00:12:02 +0530

    Update multiple_args.py

--- multiple_args.py
+++ multiple_args.py
@@ -1,3 +1,17 @@
+def mult(*n):
+  t = 0 
+  for x in n:
+    t = t+i
+  print("the sum = ")
+  return t 
+a = int(input("enter a value"))
+b= int(input("enter a value"))
+c = int(input("enter a value"))
+d = int(input("enter a value"))
+mult()
+mult(a,b,c,d)
+mult(1,2,3,4,5,2,244,34,5,6)
+
 
 
 
