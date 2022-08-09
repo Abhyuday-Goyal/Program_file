b3df8e39188588666d919f38109b6eae32b13678
Abhyuday-Goyal <107510592+Abhyuday-Goyal@users.noreply.github.com>
2022-08-10 00:08:00 +0530

    Update keyword_args.py

--- keyword_args.py
+++ keyword_args.py
@@ -1,1 +1,12 @@
+def args(age, salary):
+  if age> 18:
+    if salary>30000:
+      print("accepted")
+    else:
+      print("not accepted")
+  else:
+    print("not accepted")
+a = int(input("enter age "))
+b = int(input("enter salary"))
+args(salary = b, age = a )
 
