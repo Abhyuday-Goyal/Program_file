cc88f49e46668656265491d9be12460540f4ad59
Abhyuday-Goyal <107510592+Abhyuday-Goyal@users.noreply.github.com>
2022-08-12 23:18:57 +0530

    Update Default_arg.py

--- Default_arg.py
+++ Default_arg.py
@@ -2,3 +2,4 @@ def default( place = "india"):
   return place 
 default()
 default( France)
+# default values do not require us to give arguements to the function as they refer to the default value 
