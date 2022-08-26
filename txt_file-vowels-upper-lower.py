413350c042c3f23b5a351d64274439e83261b961
Abhyuday-Goyal <abhyuday@ainorthstartech.com>
2022-08-26 23:49:23 +0530

    displaying all counters for vowels, uppercase characters, lowercase characters, consonants

--- txt_file-vowels-upper-lower.py
+++ txt_file-vowels-upper-lower.py
@@ -4,16 +4,19 @@ vowels = 0
 upper = 0
 lower = 0 
 consonants = 0
-vowels = ['a','e','i','o','u']
+vowel_list = ['a','e','i','o','u']
 for char in file_contents:
-  if char.lower() in vowels:
-    vowel = vowel + 1
+  if char.lower() in vowel_list:
+    vowels = vowels + 1
   elif char.isalpha():
     consonants +=1 
   if char.islower():
     lower += 1
   if char.isupper():
     upper+= 1 
-  
+print(vowels)
+print(upper)
+print(lower)
+print(consonants)
   
   
