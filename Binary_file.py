9d8eaf11cd4758f2bb90fc2757993cb8d590ebf7
Abhyuday Goyal <107510592+Abhyuday-Goyal@users.noreply.github.com>
2022-08-26 23:02:56 +0530

    Update Binary_file.py

--- Binary_file.py
+++ Binary_file.py
@@ -1,9 +1,19 @@
-f = open("a.txt", "r")
+'''f = open("a.txt", "r")
 l = f.readlines()
 for i in line:
   
   w = i.split()
   for w1 in w:
     print(w1+#, end = " ")
-    print(" ")
+    print(" ")'''
+def binary():
+  import pickle 
+  f = open("ab.dat", "wb")
+  d = { "name" : Arun, "roll no." : 1, "marks": 95}
+  picle.dump(d,f)
+  f.close()
+binary()
+
+    
+  
     
