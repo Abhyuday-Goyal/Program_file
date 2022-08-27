b2cd6ebb97a852ba47da86e5d6a4b9f8d16fd9cd
Abhyuday-Goyal <abhyuday@ainorthstartech.com>
2022-08-27 16:45:26 +0530

    If line starts with 'i' or 'I' add to another file

---
+++ txt_file_starting_letters_code.py
@@ -0,0 +1,18 @@
+f = open('a.txt','r')
+lines = f.readlines()
+words = [ 'i','I']
+new_words =[]
+for i in lines:
+    if i[0] in words:
+        new_words.append(i)
+f.close()
+a = open('new.txt', 'w')
+b = a.writelines(new_words)
+a.close()
+c = open('new.txt', 'r')
+d = c.readlines()
+c.close()
+print(d)
+
+        
+    
