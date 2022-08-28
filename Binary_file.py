5b05241cff1b2f0691bf8bc25576f78d3595dd10
Abhyuday-Goyal <abhyuday@ainorthstartech.com>
2022-08-28 17:45:24 +0530

    flights problem completed

--- Binary_file.py
+++ Binary_file.py
@@ -1,18 +1,13 @@
-'''f = open("a.txt", "r")
-l = f.readlines()
-for i in line:
-  
-  w = i.split()
-  for w1 in w:
-    print(w1+#, end = " ")
-    print(" ")'''
-def binary():
-  import pickle 
-  f = open("ab.dat", "wb")
-  d = { "name" : Arun, "roll no." : 1, "marks": 95}
-  pickle.dump(d,f)
-  f.close()
-binary()
+
+
+import pickle 
+f = open("ab.dat", "wb")
+d = { "name" : 'Arun', "roll no." : 1, "marks": 95}
+pickle.dump(d,f)
+f.close()
+f1 =open("ab.dat","rb")
+lines = pickle.load(f1)
+
 
     
   
--- binary_file_flights.py
+++ binary_file_flights.py
@@ -1,12 +1,19 @@
 import os
 import pickle
-f = open('flights.dat', 'rb')
-flight1 = {{ 'flight no.':1, 'airline name':'indigo','Departure':'India','destination':'dubai'},
-{'flight no.':2, 'airline name':'jet airways','Departure':'USA','destination':'dubai'},
- {'flight no.':3, 'airline name':'indigo','Departure':'India','destination':'USA'},
-{'flight no.':4, 'airline name':'indigo','Departure':'India','destination':'Spain'}}
-pickle.dump(flight1,f)
-pickle.dump(flight2,f)
-pickle.dump(flight3,f)
-pickle.dump(flight4,f)
-if flight1["destination"] = 
+f = open('flights.dat', 'wb')
+flight1 = [{'flight no.': 1, 'airline name': 'indigo', 'Departure': 'India', 'destination': 'dubai'},
+           {'flight no.': 2, 'airline name': 'jet airways',
+               'Departure': 'USA', 'destination': 'dubai'},
+           {'flight no.': 3, 'airline name': 'indigo',
+               'Departure': 'India', 'destination': 'USA'},
+           {'flight no.': 4, 'airline name': 'indigo', 'Departure': 'India', 'destination': 'Spain'}]
+pickle.dump(flight1, f)
+f.close()
+f1 = open('flights.dat', 'rb')
+flight_list = pickle.load(f1)
+print(flight_list)
+flight_count = 0
+for flight in flight_list:
+    if flight['destination'] == 'dubai':
+        flight_count += 1
+print(flight_count)
