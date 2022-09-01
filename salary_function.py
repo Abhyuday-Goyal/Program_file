d6f8c5baea29f1324038cccc2909b7214bb97891
Abhyuday-Goyal <abhyuday@ainorthstartech.com>
2022-09-01 23:35:07 +0530

    c2

--- salary_function.py
+++ salary_function.py
@@ -1,8 +1,7 @@
 #Create a function showEmployee() in such a way that it should accept employee name, and it’s salary and display both, and if the salary is missing in function call it should show it as 30000
 
 def showEmployee(name, salary = 3000):
-  return name, salary 
-a = str(input("Enter employee name"))
-b = int(input("Enter the salary of employee"))
-showEmployee(a,b)
+  print('name of employee:', name)
+  print('salary of employee;', salary)
+  
 
