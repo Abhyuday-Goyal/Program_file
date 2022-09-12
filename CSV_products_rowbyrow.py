4428441b684726a7107dc210bd0ef7f57e2686f7
Abhyuday-Goyal <abhyuday@ainorthstartech.com>
2022-09-12 12:07:59 +0530

    12

--- CSV_products_rowbyrow.py
+++ CSV_products_rowbyrow.py
@@ -5,7 +5,7 @@ rows = [[123,'iphone',65,200000],
         [125,'ipad',10,100000]]
 filename = 'products.csv'
 with open(filename,'w', newline ='') as f:
-    csv_w = csv.writer(f, delimiter=',')
+    csv_w = csv.writer(f, delimiter='|')
     csv_w .writerow(fields)
     for i in rows:
         csv_w.writerow(i)
